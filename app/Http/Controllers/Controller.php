ce9b5b02d2c62730af4e16df0e666600efd2a53e
jhunter7333 <jhunter7333@gmail.com>
2026-04-26 21:12:37 +1000

    chore(controllers): wire AuthorizesRequests trait into base Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
 // V2: Base controller class. All API controllers extend this abstract class.
 abstract class Controller
 {
-    //
+    use AuthorizesRequests;
 }
